93543bb8551733f0e4e7260c24035d27ba1f3789
Andrii <vlasandrey878@gmail.com>
2020-01-23 10:50:55 +0200

    add product component

--- src/app/admin/product/product.module.ts
+++ src/app/admin/product/product.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductItemComponent } from './product-item/product-item.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ProductItemComponent, ProductListComponent],
   imports: [
     CommonModule
   ]
